979644f27bbca83b8721d54b856020d23af836e5
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-08-08 14:10:02 +0100

    Implement SearchInput to search through Companion Libary

---
+++ components/SearchInput.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+
+const SearchInput = () => {
+  const pathname = usePathname();
+  console.log("Current Pathname:", pathname);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const deleayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === "/companions") {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+  }, [searchQuery, router, searchParams, pathname]);
+
+  return (
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 h-fit">
+      <Image src="/icons/search.svg" alt="Search" width={15} height={15} />
+      <Input
+        placeholder="Search companions ..."
+        className="border-0 focus-visible:ring-0 focus-visible:border-0 shadow-none w-full"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
