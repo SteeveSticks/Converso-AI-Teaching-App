64547f51e471c46e21613e37449017fca79ef23c
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-08-20 22:01:37 +0100

    Implment Companions Single Page ID, Implement Vapi Session with Chatgpt Promt, Implement Sentry Application Setup

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import CompanionComponent from "@/components/CompanionComponent";
+import { getCompanion } from "@/lib/companions.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const CompanionSession = () => {
-  return <div>CompanionSession</div>;
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
+}
+
+// params /url/{id}
+// searchParams /url?key=value&key1=value1
+
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const { name, subject, topic, duration } = companion;
+
+  const user = await currentUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  if (!name || !subject || !topic || !duration) {
+    redirect("/companions");
+  }
+
+  return (
+    <main>
+      <article className="flex rounded-border justify-between gap-30 p-5 max-w-3xl mx-auto">
+        <div className="flex items-center gap-2">
+          <div
+            className="size-[52px] flex items-center justify-center rounded-lg max-md:hidden"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              width={35}
+              height={35}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-xl">{name}</p>
+              <div className="bg-black text-white rounded-4xl text-[11px] px-2 py-1 capitalize max-sm:hidden">
+                {subject}
+              </div>
+            </div>
+            <p className="text-base">{topic}</p>
+          </div>
+        </div>
+
+        <div className="items-start text-xl max-md:hidden">
+          {duration} minutes
+        </div>
+      </article>
+
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
+    </main>
+  );
 };
 
 export default CompanionSession;
--- lib/companions.actions.ts
+++ lib/companions.actions.ts
@@ -52,3 +52,17 @@ export const getAllCompanions = async ({
 
   return companions;
 };
+
+export const getCompanion = async (id: string) => {
+  const supabase = createSupabaseClient();
+
+  // Fetching a single companion by ID
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("id", id);
+
+  if (error) return console.log(error);
+
+  return data[0];
+};
