bae8873aceed98c83b0a8c5cbd670f3e8d58d15e
Stephen Adebanjo <stephenadebanjo86@gmail.com>
2025-09-29 02:13:40 +0100

    Clerk Subscription Working setup: making clerk subscription work perfectly to the exact rules given

--- lib/companions.actions.ts
+++ lib/companions.actions.ts
@@ -128,3 +128,35 @@ export const getUserCompanions = async (userId: string) => {
 
   return data;
 };
+
+export const newCompanionPermission = async () => {
+  const { userId, has } = await auth();
+  const supabase = createSupabaseClient();
+
+  let limit = 0;
+
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ feature: "10_companion_limit" })) {
+    limit = 10;
+  } else if (has({ feature: "3_companion_limit" })) {
+    limit = 3;
+  }
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+
+  if (error) throw new Error(error.message);
+
+  const companionCount = data?.length;
+
+  // if the list is set and the companion equal or exceeds it
+  // deny permission
+  if (limit > 0 && companionCount >= limit) {
+    return false;
+  } else {
+    return true;
+  }
+};
